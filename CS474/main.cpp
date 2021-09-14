c97273e0e5f0ab19ce59a49ee2b05231b0b9210f
Mark McCord <43460073+MarkMcCord@users.noreply.github.com>
2021-09-13 22:56:43 -0700

    Add files via upload

---
+++ CS474/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+
+#include "image.h"
+
+using namespace std;
+
+void readImage(char fname[], ImageType& image);
+void writeImage(char fname[], ImageType& image);
+
+int main(int argc, char * argv[])
+{
+    return 0;
+}
