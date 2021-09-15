4392064f0ea167c850a7a161ec69724a2bd57795
joshinsorio <joshuainsorio@outlook.com>
2021-09-15 00:22:02 -0700

    temp

--- CS474/main.cpp
+++ CS474/main.cpp
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <fstream>
 #include <sstream>
+#include <cmath>
+#include <algorithm>
 
 #include "image.h"
 
@@ -13,6 +15,7 @@ using namespace std;
 void readImage(char fname[], ImageType& image);
 void writeImage(char fname[], ImageType& image);
 void sample (char fname[]);
+void eqHistogram(char fname[]);
 
 int main(int argc, char * argv[])
 {
@@ -24,12 +27,79 @@ int main(int argc, char * argv[])
     writeImage(argv[2], image);*/
 
     //part 1
-    char lenna[] = "lenna.pgm";
-    char peppers[] = "peppers.pgm";
-    sample(lenna);
-    sample(peppers);
+    // char lenna[] = "lenna.pgm";
+    // char peppers[] = "peppers.pgm";
+    // sample(lenna);
+    // sample(peppers);
+
+    //part 3
+    char boat[] = "boat.pgm";
+    char f_16[] = "f_16.pgm";
+    eqHistogram(boat);
 
     return 0;
+}
+
+void eqHistorgram(char fname[])
+{
+    ImageType image(256, 256, 255);
+    readImage(fname, image);
+
+    int n_bins = 255 + 1;
+    int total = 256 * 256;
+
+    // Compute histogram
+    vector<int> hist(n_bins, 0);
+    for (int i = 0; i < 256; i++) {
+        for(int j = 0; j < 256; j++){
+            int temp;
+            hist[image.getPixelVal(i, j, temp),0]++;
+        }
+    }
+
+    //Find first nonzero bin
+    int i = 0;
+    while(!hist[i]) ++i;
+
+    if (hist[i] == total) {
+        for (int j = 0; j < 256; j++) { 
+            for(int k = 0; k < 256; k++){
+                int temp;
+                image.setPixelVal(j, k, i); 
+            }
+        }
+        return;
+    }
+
+    // Compute scale
+    float scale = (n_bins - 1.f) / (total - hist[i]);
+
+    // Initialize lut
+    vector<int> lut(n_bins, 0);
+    i++;
+
+    int sum = 0;
+    for (; i < hist.size(); ++i) {
+        sum += hist[i];
+        // the value is saturated in range [0, max_val]
+        lut[i] = max(0, min(int(round(sum * scale)), 255));
+    }
+
+    ImageType equalizedImage(256, 256, 255);
+
+    // Apply equalization
+    for (int i = 0; i < 256; i++) {
+        for(int j = 0; j < 256; j++){
+            int temp;
+            equalizedImage.setPixelVal(i, j, lut[image.getPixelVal(i,j,temp), 0]);
+        }
+    }
+    char name[] = "in.pgm";
+    name[0] = '0' + i;
+    name[1] = fname[0];
+
+    writeImage(name, equalizedImage);
+
 }
 
 void sample(char fname[]){
